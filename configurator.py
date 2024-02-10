a9474e873ec7eca48cefeac1598ef8bbe8e0f3ee
Ethan C <ecape0207@gmail.com>
2024-02-09 22:29:28 -0700

    changed how to install cuda

--- configurator.py
+++ configurator.py
@@ -1,29 +1,26 @@
 import os
 
 def install_nvidia_drivers():
-    os.system("apt update && apt upgrade -y")
-    os.system("ubuntu-drivers autoinstall")
+    os.system("sudo apt update && sudo apt upgrade -y")
+    os.system("sudo apt install ubuntu-drivers-common")
+    os.system("sudo apt install nvidia-driver-535")
 
 def install_cuda_drivers():
+    os.system("sudo apt install gcc")
     os.system("wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb")
     os.system("sudo dpkg -i cuda-keyring_1.1-1_all.deb")
-    os.system("apt update")
-    os.system("apt install cuda -y")
+    os.system("sudo apt update")
+    os.system("sudo apt install cuda -y")
 
 def install_ssh():
     os.system("sudo apt install openssh-server")
     os.system("sudo systemctl enable ssh")
 
-def install_gpu_burn():
-    os.system("sudo apt install g++")
-    os.system("git clone https://github.com/wilicc/gpu-burn.git")
-    os.system("cd gpu-burn && make")
 
 def main():
     install_nvidia_drivers()
     install_cuda_drivers()
     install_ssh()
-    install_gpu_burn()
     print("Please reboot your machine")
 
 if __name__ == '__main__':
